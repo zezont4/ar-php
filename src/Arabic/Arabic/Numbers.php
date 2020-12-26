26c545b029fd2d9743e0227693951b29ee294cd6
Abdulaziz Altayyar <zezont@gmail.com>
2020-12-26 14:07:00 +0300

    Update Numbers.php
    Change class name from "I18N_Arabic_Numbers" to Numbers

--- src/Arabic/Arabic/Numbers.php
+++ src/Arabic/Arabic/Numbers.php
@@ -123,7 +123,7 @@
  * @license   LGPL <http://www.gnu.org/licenses/lgpl.txt>
  * @link      http://www.ar-php.org 
  */ 
-class I18N_Arabic_Numbers
+class Numbers
 {
     private $_individual    = array();
     private $_complications = array();
